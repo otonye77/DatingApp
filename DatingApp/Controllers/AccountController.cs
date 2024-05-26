63219840038b06f584606ddfb1da1e9c285397a2
otonye77 <oamietubodie@gmail.com>
2024-05-26 16:09:39 +0100

    Adding Dto

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using DatingApp.Data;
+using DatingApp.DTOs;
 using DatingApp.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -17,18 +19,26 @@ namespace DatingApp.Controllers
         }
 
         [HttpPost("register")] //api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExists(registerDto.UserName))
+            {
+                return BadRequest("Username is taken");
+            }
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);   
             await _context.SaveChangesAsync();
             return Ok(user);    
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
